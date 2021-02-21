222d8f7f5d0bbdba6553eb704443bfb05f4135e8
Matthew Lake <matt@matthewlake.co.uk>
2021-02-21 19:48:38 +0000

    Add getter and setter for template directory

--- TemplateLoader.php
+++ TemplateLoader.php
@@ -4,4 +4,16 @@ namespace Trunk\Template;
 
 class TemplateLoader
 {
+    private string $templateDirectory = '/views/';
+
+    public function setTemplateDirectory(string $url): self
+    {
+        $this->templateDirectory = $url;
+        return $this;
+    }
+
+    public function getTemplateDirectory(): string
+    {
+        return $this->templateDirectory;
+    }
 }
