239d44f1eff6c096f29eae3f98a7797885ca135b
Matthew Lake <matt@matthewlake.co.uk>
2021-02-21 21:35:50 +0000

    Fix typo in test description

--- tests/UrlParserTest.php
+++ tests/UrlParserTest.php
@@ -8,7 +8,7 @@ it('adds trailing slash to template directory string if missing', function () {
     expect(UrlParser::formatTemplateDirectory('directory'))->toBe('directory/');
 });
 
-it('does not add traling slash if the template directory already contains one', function () {
+it('does not add trailing slash if the template directory already contains one', function () {
     expect(UrlParser::formatTemplateDirectory('directory/'))->toBe('directory/');
 });
 
